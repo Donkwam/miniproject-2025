533a2e97b3ea30dce6cdf9b0a38886b2a8d99d4b
Woo Dong Kwan <ehdrhks0509@naver.com>
2025-06-18 09:52:23 +0900

    Update MainViewModel.cs

--- miniproject2/mes/MesMiniproject/WpfioTSimulatorApp/ViewModels/MainViewModel.cs
+++ miniproject2/mes/MesMiniproject/WpfioTSimulatorApp/ViewModels/MainViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Media;
 
 namespace WpfIoTSimulatorApp.ViewModels
 {
     public partial class MainViewModel : ObservableObject
     {
         private string _greeting;
+        // 색상표시할 변수
+        private Brush _productBrush;
 
         public MainViewModel()
         {
@@ -15,6 +19,22 @@ namespace WpfIoTSimulatorApp.ViewModels
         {
             get => _greeting;
             set => SetProperty(ref _greeting, value);
+        }
+
+        public Brush ProductBrush
+        {
+            get => _productBrush;
+            set => SetProperty(ref _productBrush, value);
+        }
+
+        public event Action? StartHmiRequested;
+        public event Action? StartSensorCheckRequested; // VM에서 View에 있는 이벤트를 호출
+
+        [RelayCommand]
+        public void Test()
+        {
+            ProductBrush = Brushes.Gray;
+            StartHmiRequested?.Invoke(); // 컨베이어벨트 애니메이션 요청(View에서 처리)
         }
     }
 }
